e485fea461cae21557d4ad7ff45bcb4feedb3d03
antonio pacehco <antonio_pacheco@outlook.com>
2020-04-26 14:05:31 -0700

    fix use off for realtime database

--- src/index.js
+++ src/index.js
@@ -37,52 +37,56 @@ export const LiveViewer = ({ db, immediate = false, ...props }) => {
   let simulating = false;
   let drawImmediate = immediate;
 
-  useEffect(() => {
-    const unsubscribe_child = db.on("child_removed", () => {
-      canvasRef.clear();
-    });
+  const clearCanvasRef = () => {
+    canvasRef.clear();
+  };
 
-    const unsubscribe_lines = db.child("lines").on("child_added", (snap) => {
-      const line = snap.val();
-      const { points, brushColor, brushRadius } = line;
-      let curTime = 0;
-      drawImmediate = simulating ? true : immediate;
-      let timeoutGap = drawImmediate ? 0 : canvasRef.props.loadTimeOffset;
+  const onChildAdded = (snap) => {
+    const line = snap.val();
+    const { points, brushColor, brushRadius } = line;
+    let curTime = 0;
+    drawImmediate = simulating ? true : immediate;
+    let timeoutGap = drawImmediate ? 0 : canvasRef.props.loadTimeOffset;
 
-      if (drawImmediate) {
+    if (drawImmediate) {
+      canvasRef.drawPoints({
+        points,
+        brushColor,
+        brushRadius,
+      });
+      canvasRef.points = points;
+      canvasRef.saveLine({ brushColor, brushRadius });
+      return;
+    }
+    // Use timeout to draw
+    simulating = true;
+    for (let i = 1; i < points.length; i++) {
+      curTime += timeoutGap;
+      window.setTimeout(() => {
         canvasRef.drawPoints({
-          points,
+          points: points.slice(0, i + 1),
           brushColor,
           brushRadius,
         });
-        canvasRef.points = points;
-        canvasRef.saveLine({ brushColor, brushRadius });
-        return;
-      }
-      // Use timeout to draw
-      simulating = true;
-      for (let i = 1; i < points.length; i++) {
-        curTime += timeoutGap;
-        window.setTimeout(() => {
-          canvasRef.drawPoints({
-            points: points.slice(0, i + 1),
-            brushColor,
-            brushRadius,
-          });
-        }, curTime);
-      }
-
-      curTime += timeoutGap;
-      window.setTimeout(() => {
-        // Save this line with its props
-        canvasRef.points = points;
-        canvasRef.saveLine({ brushColor, brushRadius });
-        simulating = false;
       }, curTime);
-    });
+    }
+
+    curTime += timeoutGap;
+    window.setTimeout(() => {
+      // Save this line with its props
+      canvasRef.points = points;
+      canvasRef.saveLine({ brushColor, brushRadius });
+      simulating = false;
+    }, curTime);
+  };
+
+  useEffect(() => {
+    db.on("child_removed", clearCanvasRef);
+    db.child("lines").on("child_added", onChildAdded);
+
     return () => {
-      unsubscribe_child();
-      unsubscribe_lines();
+      db.off("child_removed", clearCanvasRef);
+      db.child("lines").off("child_added", onChildAdded);
     };
   }, []);
 
@@ -97,13 +101,16 @@ export const LiveViewer = ({ db, immediate = false, ...props }) => {
 
 export const RecorderViewer = ({ db, immediate = false, ...props }) => {
   let canvasRef = useRef(null);
+
+  const onValue = (snap) => {
+    const draws = snap.val();
+    canvasRef.loadSaveData(JSON.stringify(draws), immediate);
+  };
+
   useEffect(() => {
-    const unsubscribe = db.once("value", (snap) => {
-      const draws = snap.val();
-      canvasRef.loadSaveData(JSON.stringify(draws), immediate);
-    });
+    db.once("value", onValue);
     return () => {
-      unsubscribe();
+      db.off("value", onValue);
     };
   }, []);
 
